4eb01c501d0a83c7cd6a7776207dfb2200e03049
ayman <ayman.sadaf08@gmail.com>
2025-09-03 15:08:48 -0400

    FINISHED THE EXAMPLES OF SECTION 1

--- pythonSyntax.py
+++ pythonSyntax.py
@@ -14,7 +14,7 @@ Explanations and examples will be added by Greg.
 # - Multi-line comments (docstrings)
 # ==========================================================
 
-
+print("Sadaf",3.14,18)
 
 # ==========================================================
 # 2. VARIABLES AND DATA TYPES
@@ -25,7 +25,18 @@ Explanations and examples will be added by Greg.
 # - Type checking with type()
 # ==========================================================
 
+message = "Welcome to FIU"
 
+print(type(message))
+
+a= 10
+b= 2
+print(type(a),type(b))
+
+isOpen = True
+print(type(isOpen))
+
+print(message[0])
 
 # ==========================================================
 # 3. BASIC OPERATORS
@@ -35,7 +46,11 @@ Explanations and examples will be added by Greg.
 # - Logical (and, or, not)
 # ==========================================================
 
-
+print(a+b)
+print(a*b)
+print(7/2)
+print(7//2)
+print(7%2)
 
 # ==========================================================
 # 4. STRING OPERATIONS
@@ -45,7 +60,12 @@ Explanations and examples will be added by Greg.
 # - Common methods (.upper(), .lower(), .strip(), etc.)
 # ==========================================================
 
+first_name = "ayman"
+last_name = "sadaf"
 
+print(first_name+" "+last_name)
+print(f"My name is {first_name.upper()} {last_name.title()}.")
+print("***Welcome to Software Dev***".strip('*'))
 
 # ==========================================================
 # 5. LISTS
@@ -56,8 +76,26 @@ Explanations and examples will be added by Greg.
 # - Useful methods (.append(), .remove(), .sort(), etc.)
 # ==========================================================
 
+professors = ["greg","joe", "richard","johnson","jason","leo","heather"]
+print(type(professors))
+print(professors)
+print(professors[2:4]) #accessing indices 2,3. Upper bound never included
+print(professors[:5]) #accessing till place 5
+print(professors[2:])
+print(professors[:]) #better way to access and edit list becs original is copied and not affected.
 
-
+professors.append("todd")
+professors.extend(["robinson","Stockholm","mustafa"])
+professors.insert(2,"vyoma")
+print(professors)
+x=professors.pop(2)
+print(x)
+professors.reverse()
+print(professors)
+professors.sort()
+print(professors)
+professors.sort(reverse=True)
+print(professors)
 # ==========================================================
 # 6. TUPLES AND SETS
 # ----------------------------------------------------------
@@ -65,7 +103,12 @@ Explanations and examples will be added by Greg.
 # - Sets: unique collections
 # ==========================================================
 
+grades =(88.3,78.6,99.5)
+print(type(grades))
+# grades [0]= 91.3
 
+members = {"greg","richard","greg"}
+print(members)
 
 # ==========================================================
 # 7. DICTIONARIES
